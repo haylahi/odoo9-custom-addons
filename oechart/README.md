d3b4cd884f2b47a1e8490fa5c13df9c1bb148f1a
Alexis Manuel <alexis.manuelpro@gmail.com>
2018-05-30 15:11:39 +0200

    init web chart extension

---
+++ oechart/README.md
@@ -0,0 +1,2 @@
+# odoo9-webchart
+This is an OpenERP 9 addons to replace all chart of the web client by google chart version.
---
+++ oechart/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'OpenERP Google Chart',
+    'version': '1.0',
+    'summary': 'Enhance web charts with Google ones',
+    'category': 'Tools',
+    'description':
+        """
+OpenERP Google Chart
+=================
+
+This is an OpenERP 9 addons to replace all chart of the web client by google chart version.
+        """,
+    'data': [
+    ],
+    'depends' : [],
+    'qweb': [],
+    'application': True,
+}
