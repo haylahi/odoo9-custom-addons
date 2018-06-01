af05d9f96b2e7ce127e0be0af85c3e2b44fabeac
Alexis Manuel <alexis.manuelpro@gmail.com>
2018-06-01 12:13:49 +0200

    location added to the tested model

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -37,6 +37,7 @@ class Course(models.Model):
 class Session(models.Model):
     _name = "openacademy.session"
     name = fields.Char(required=True)
+    location = fields.Char(string="Location")
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
