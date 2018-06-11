95fe05cde2ffb66561d47955e160df56d111a456
Alexis Manuel <alexis.manuelpro@gmail.com>
2018-06-11 09:53:58 +0200

    refactor indent

--- openBI/static/src/js/views/graph_view.js
+++ openBI/static/src/js/views/graph_view.js
@@ -1,155 +1,156 @@
 odoo.define('oechart.GraphView',function (require) {
-    "use strict";
+  "use strict";
 
-    var core = require('web.core');
-    var GraphView = require('web.GraphView')
+  var core = require('web.core');
+  var GraphView = require('web.GraphView')
 
-    var _t = core._t;
-    var QWeb = core.qweb;
+  var _t = core._t;
+  var QWeb = core.qweb;
 
+  /**
+   * Extend the web.GraphView widget with the include method.
+   */
+  GraphView.include({
+    events: {
+      'change .mapView .panel input': 'build_options',
+      'change .mapView .panel select': function () {
+        this.select_set_state();
+        this.build_options();
+        //this.sort_options();
+      },
+      /**
+       * Get the displayed map and creates a new page to print.
+       */
+      'click .mapView #printMap': () => {
+        let mapWindow = window.open('', 'PRINT', 'height=400,width=600');
+        mapWindow.document.write(document.getElementById('map_div').innerHTML);
+        mapWindow.document.close();
+        mapWindow.focus();
+        mapWindow.print();
+        mapWindow.close();
+      },
+    },
+    sort_options: () => {
+      let sorted;
+      $(".mapView .select-sortable")
+        .map(function () { 
+          sorted = $(this)
+                      .children()
+                      .sort(function(a,b) {
+                        if (a.text > b.text) return 1;
+                        else if (a.text < b.text) return -1;
+                        else return 0
+                      });
+          $(this)
+            .empty()
+            .append(sorted);
+        });
+    },
     /**
-     * Extend the web.GraphView widget with the include method.
+     * Creates an option data structure to update the map based on user inputs.
      */
-    GraphView.include({
-        events: {
-            'change .mapView .panel input': 'build_options',
-            'change .mapView .panel select': function () {
-                this.select_set_state();
-                this.build_options();
-                //this.sort_options();
-            },
-            /**
-             * Get the displayed map and creates a new page to print.
-             */
-            'click .mapView #printMap': () => {
-                let mapWindow = window.open('', 'PRINT', 'height=400,width=600');
-                mapWindow.document.write(document.getElementById('map_div').innerHTML);
-                mapWindow.document.close();
-                mapWindow.focus();
-                mapWindow.print();
-                mapWindow.close();
-            },
-        },
-        sort_options: () => {
-            let sorted;
-            $(".mapView .select-sortable")
-                .map(function () { 
-                    sorted = $(this)
-                                .children()
-                                .sort(function(a,b) {
-                                    if (a.text > b.text) return 1;
-                                    else if (a.text < b.text) return -1;
-                                    else return 0
-                                });
-                    $(this)
-                        .empty()
-                        .append(sorted);
-                });
-        },
-        /**
-         * Creates an option data structure to update the map based on user inputs.
-         */
-        build_options: function () {
-            let optionStruct = {};
-            _.each($(".panel .list-group select"), e => {
-                optionStruct[$(e).attr("name")] = $(e).val();
-            });
-            _.each($(".panel .list-group input"), e => {
-                optionStruct[$(e).attr("name")] = $(e).val();
-            });
-            this.widget.updateMap(optionStruct);
-        },
-        /**
-         * Manages 'Display settings' select input states at each value change
-         */
-        select_set_state: () => {
-            // Show subcontinents if 'All' isn't selected
-            if($('.select-continents').val() !== "world") {
+    build_options: function () {
+      let optionStruct = {};
+      _.each($(".panel .list-group select"), e => {
+        optionStruct[$(e).attr("name")] = $(e).val();
+      });
+      _.each($(".panel .list-group input"), e => {
+        optionStruct[$(e).attr("name")] = $(e).val();
+      });
+      this.widget.updateMap(optionStruct);
+    },
+    /**
+     * Manages 'Display settings' select input states at each value change
+     */
+    select_set_state: () => {
+      // Show subcontinents if 'All' isn't selected
+      if($('.select-continents').val() !== "world") {
 
-                // Set selected subcontinent to default if it is from an other continent
-                if($('.select-subcontinents').find(':selected').data("tag") != $('.select-continents').val()) {
-                    $('.select-subcontinents').val(" ");
-                }
-
-                // Set selected country to default if it is from an other subcontinent or continent
-                if($('.select-countries').find(':selected').data("ctag") != $('.select-continents').val() || ($('.select-countries').find(':selected').data("tag") != $('.select-subcontinents').val() && $('.select-subcontinents').val() != " ")) {
-                    $('.select-countries').val(" ");
-                } 
-
-                // Show sub-continents select and options
-                $('.select-subcontinents').parent().removeClass("hidden");
-                $('.select-subcontinents').parent().show();
-                $('.select-subcontinents option').show();
-                
-                // Hide all the subcontinents not from the selected continent
-                $('.select-subcontinents option')
-                    .filter(function () { return $(this).data("tag") != $('.select-continents').val() && $(this).val() != " " })
-                    .hide();
-                
-                // Show countries select and options
-                $('.select-countries').parent().removeClass("hidden");
-                $('.select-countries').parent().show();
-                $('.select-countries option').show();
-
-                // Hide all the countries not from the selected subcontinent or continent
-                if ($('.select-subcontinents').val() != " ") {
-                    $('.select-countries option')
-                        .filter(function () { return $(this).data("tag") != $('.select-subcontinents').val() && $(this).val() != " " })
-                        .hide();
-                } else {
-                    $('.select-countries option')
-                    .filter(function () { return $(this).data("ctag") != $('.select-continents').val() && $(this).val() != " " })
-                    .hide();
-                }
-            } else {
-                // Hide subcontinents and country select if 'All' is chosen
-                $('.select-subcontinents').val(" ");
-                $('.select-subcontinents').parent().addClass("hidden");
-                $('.select-countries').val(" ");
-                $('.select-countries').parent().addClass("hidden");
-            }
-        },
-        /**
-         * @param $node
-         * 
-         * Override web.GraphView.render_buttons method with new buttons rendering 
-         * (the geo chart button is rendered in addition to existing ones)
-         */
-        render_buttons: function ($node) {
-            if ($node) {
-                var context = {measures: _.pairs(_.omit(this.measures, '__count__'))};
-                this.$buttons = $(QWeb.render('newGraphView.buttons', context));
-                this.$measure_list = this.$buttons.find('.oe-measure-list');
-                this.update_measure();
-                this.$buttons.find('button').tooltip();
-                this.$buttons.click(this.on_button_click.bind(this));
-    
-                this.$buttons.appendTo($node);
-            }
-        },
-        /**
-         * @param event object containing the event target
-         * 
-         * Override web.GraphView.on_button_click method with new events binding
-         * (bind the display_geo method in graph_widget.js to the geo chart button)
-         */
-        on_button_click: function (event) {
-            var $target = $(event.target);
-            if ($target.hasClass('oe-bar-mode')) {this.widget.set_mode('bar');}
-            if ($target.hasClass('oe-line-mode')) {this.widget.set_mode('line');}
-            if ($target.hasClass('oe-pie-mode')) {this.widget.set_mode('pie');}
-            if ($target.hasClass('oe-geo-mode')) {this.widget.set_mode('geo');}
-            if ($target.parents('.oe-measure-list').length) {
-                var parent = $target.parent(),
-                    field = parent.data('field');
-                this.active_measure = field;
-                parent.toggleClass('selected');
-                event.stopPropagation();
-                this.update_measure();
-                this.widget.set_measure(this.active_measure);
-            }
+        // Set selected subcontinent to default if it is from an other continent
+        if($('.select-subcontinents').find(':selected').data("tag") != $('.select-continents').val()) {
+          $('.select-subcontinents').val(" ");
         }
-    })
+
+        // Set selected country to default if it is from an other subcontinent or continent
+        if($('.select-countries').find(':selected').data("ctag") != $('.select-continents').val() || ($('.select-countries').find(':selected').data("tag") != $('.select-subcontinents').val() && $('.select-subcontinents').val() != " ")) {
+          $('.select-countries').val(" ");
+        } 
+
+        // Show sub-continents select and options
+        $('.select-subcontinents').parent().removeClass("hidden");
+        $('.select-subcontinents').parent().show();
+        $('.select-subcontinents option').show();
+        
+        // Hide all the subcontinents not from the selected continent
+        $('.select-subcontinents option')
+          .filter(function () { return $(this).data("tag") != $('.select-continents').val() && $(this).val() != " " })
+          .hide();
+        
+        // Show countries select and options
+        $('.select-countries').parent().removeClass("hidden");
+        $('.select-countries').parent().show();
+        $('.select-countries option').show();
+
+        // Hide all the countries not from the selected subcontinent or continent
+        if ($('.select-subcontinents').val() != " ") {
+          $('.select-countries option')
+            .filter(function () { return $(this).data("tag") != $('.select-subcontinents').val() && $(this).val() != " " })
+            .hide();
+        } else {
+          $('.select-countries option')
+            .filter(function () { return $(this).data("ctag") != $('.select-continents').val() && $(this).val() != " " })
+            .hide();
+        }
+      } else {
+          // Hide subcontinents and country select if 'All' is chosen
+          $('.select-subcontinents').val(" ");
+          $('.select-subcontinents').parent().addClass("hidden");
+          $('.select-countries').val(" ");
+          $('.select-countries').parent().addClass("hidden");
+      }
+    },
+    /**
+     * @param $node
+     * 
+     * Override web.GraphView.render_buttons method with new buttons rendering 
+     * (the geo chart button is rendered in addition to existing ones)
+     */
+    render_buttons: function ($node) {
+        if ($node) {
+          var context = {measures: _.pairs(_.omit(this.measures, '__count__'))};
+          this.$buttons = $(QWeb.render('newGraphView.buttons', context));
+          this.$measure_list = this.$buttons.find('.oe-measure-list');
+          this.update_measure();
+          this.$buttons.find('button').tooltip();
+          this.$buttons.click(this.on_button_click.bind(this));
+
+          this.$buttons.appendTo($node);
+        }
+    },
+    /**
+     * @param event object containing the event target
+     * 
+     * Override web.GraphView.on_button_click method with new events binding
+     * (bind the display_geo method in graph_widget.js to the geo chart button)
+     */
+    on_button_click: function (event) {
+        var $target = $(event.target);
+        if ($target.hasClass('oe-bar-mode')) {this.widget.set_mode('bar');}
+        if ($target.hasClass('oe-line-mode')) {this.widget.set_mode('line');}
+        if ($target.hasClass('oe-pie-mode')) {this.widget.set_mode('pie');}
+        if ($target.hasClass('oe-geo-mode')) {this.widget.set_mode('geo');}
+        if ($target.parents('.oe-measure-list').length) {
+          var parent = $target.parent(),
+            field = parent.data('field');
+            
+          this.active_measure = field;
+          parent.toggleClass('selected');
+          event.stopPropagation();
+          this.update_measure();
+          this.widget.set_measure(this.active_measure);
+        }
+    }
+  })
 });
 
 
--- openBI/static/src/js/views/graph_widget.js
+++ openBI/static/src/js/views/graph_widget.js
@@ -1,173 +1,175 @@
 odoo.define('oechart.Graphwidget', function (require) {
-    "use strict";
-    
-    var core = require('web.core');
-    var GraphWidget = require('web.GraphWidget');
-    
-    var _t = core._t;
-    var QWeb = core.qweb;
-    
-    // Global map options
-    let mapOptions = {tooltip: { trigger: 'selection' }};
+  "use strict";
+  
+  var core = require('web.core');
+  var GraphWidget = require('web.GraphWidget');
+  
+  var _t = core._t;
+  var QWeb = core.qweb;
+  
+  // Global map options
+  let mapOptions = {tooltip: { trigger: 'selection' }};
 
+  /**
+   * Extends web.GraphWidget with the include method.
+   */
+  GraphWidget.include({
     /**
-     * Extends web.GraphWidget with the include method.
+     * Main method displaying the map on geo chart button click.
      */
-    GraphWidget.include({
-        /**
-         * Main method displaying the map on geo chart button click.
-         */
-        display_geo: function () {
-            let self = this;
+    display_geo: function () {
+        let self = this;
 
-            self.initMap();
-            google.charts.setOnLoadCallback(function () {
-                self.drawRegionsMap(self.prepareDataMap());
-            });
-        },
-        /**
-         * Render the map view and load necessary packages.
-         */
-        initMap: function () {
-            mapOptions = {tooltip: { trigger: 'selection' }};
-            this.$el.empty();
-            this.$el.append(QWeb.render('GeoChartView'));
+        self.initMap();
+        google.charts.setOnLoadCallback(function () {
+          self.drawRegionsMap(self.prepareDataMap());
+          self.addMapEvents();
+        });
+    },
+    /**
+     * Render the map view and load necessary packages.
+     */
+    initMap: function () {
+        mapOptions = {tooltip: { trigger: 'selection' }};
+        this.$el.empty();
+        this.$el.append(QWeb.render('GeoChartView'));
 
-            google.charts.load('current', {
-                'packages':['geochart','table'],
-                'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-            });
-        },
-        /**
-         * @returns features transformed data
-         * 
-         * Transform data in an understandable format for the map.
-         */
-        prepareDataMap: function () {
+        google.charts.load('current', {
+          'packages':['geochart','table'],
+          'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+        });
+    },
+    /**
+     * @returns features transformed data
+     * 
+     * Transform data in an understandable format for the map.
+     */
+    prepareDataMap: function () {
 
-            // prepare data
-            let data, values,
-            measure = this.fields[this.measure].string;
+      // prepare data
+      let data, values,
+      measure = this.fields[this.measure].string;
 
-            // zero groupbys
-            if (this.groupbys.length === 0) {
-                data = [{
-                    values: [{
-                        x: measure,
-                        y: this.data[0].value}],
-                    key: measure
-                }];
-            } 
+      // zero groupbys
+      if (this.groupbys.length === 0) {
+        data = [{
+          values: [{
+            x: measure,
+            y: this.data[0].value}],
+          key: measure
+        }];
+      } 
 
-            // one groupby
-            if (this.groupbys.length === 1) {
-                values = this.data.map(function (datapt) {
-                    return {x: datapt.labels, y: datapt.value};
+      // one groupby
+      if (this.groupbys.length === 1) {
+          values = this.data.map(function (datapt) {
+            return {x: datapt.labels, y: datapt.value};
+          });
+          data = [
+            {
+              values: values,
+              key: measure,
+            }
+          ];
+      }
+      if (this.groupbys.length > 1) {
+          let xlabels = [],
+            series = [],
+            label, serie, value;
+          values = {};
+          for (var i = 0; i < this.data.length; i++) {
+            label = this.data[i].labels[0];
+            serie = this.data[i].labels[1];
+            value = this.data[i].value;
+            if ((!xlabels.length) || (xlabels[xlabels.length-1] !== label)) {
+                xlabels.push(label);
+            }
+            series.push(this.data[i].labels[1]);
+            if (!(serie in values)) {values[serie] = {};}
+            values[serie][label] = this.data[i].value;
+          }
+          series = _.uniq(series);
+          data = [];
+          let current_serie, j;
+          for (i = 0; i < series.length; i++) {
+            current_serie = {values: [], key: series[i]};
+            for (j = 0; j < xlabels.length; j++) {
+                current_serie.values.push({
+                    x: xlabels[j],
+                    y: values[series[i]][xlabels[j]] || 0,
                 });
-                data = [
-                    {
-                        values: values,
-                        key: measure,
-                    }
-                ];
             }
-            if (this.groupbys.length > 1) {
-                let xlabels = [],
-                    series = [],
-                    label, serie, value;
-                values = {};
-                for (var i = 0; i < this.data.length; i++) {
-                    label = this.data[i].labels[0];
-                    serie = this.data[i].labels[1];
-                    value = this.data[i].value;
-                    if ((!xlabels.length) || (xlabels[xlabels.length-1] !== label)) {
-                        xlabels.push(label);
-                    }
-                    series.push(this.data[i].labels[1]);
-                    if (!(serie in values)) {values[serie] = {};}
-                    values[serie][label] = this.data[i].value;
-                }
-                series = _.uniq(series);
-                data = [];
-                let current_serie, j;
-                for (i = 0; i < series.length; i++) {
-                    current_serie = {values: [], key: series[i]};
-                    for (j = 0; j < xlabels.length; j++) {
-                        current_serie.values.push({
-                            x: xlabels[j],
-                            y: values[series[i]][xlabels[j]] || 0,
-                        });
-                    }
-                    data.push(current_serie);
-                }
+            data.push(current_serie);
+          }
+      }
+
+      // Organize the data for the map
+      let features = [['Country', data[0].key]]
+      data[0].values.forEach(e => {
+        features.push([e.x[0],e.y]);
+      });
+
+      return features;
+    },
+    /**
+     * @argument dataStruct organized data
+     * @argument options object with map parameters
+     * 
+     * Draw the map and the table.
+     */
+    drawRegionsMap: function (features) {
+      let data = google.visualization.arrayToDataTable(features);
+
+      let chart = new google.visualization.GeoChart(document.getElementById('map_div'));
+      let table = new google.visualization.Table(document.getElementById('table_div'));
+
+      chart.draw(data, mapOptions);
+      table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+    },
+    /**
+     * @param optionStruct object containing user inputs
+     *  
+     * Redraw the map
+     */
+    updateMap: function(optionStruct) {
+      this.prepareOptions(optionStruct);
+      this.drawRegionsMap(this.prepareDataMap());
+    },
+    /**
+     * @param optionStruct object containing user inputs
+     * 
+     * Transforms user input into a map configuration
+     */
+    prepareOptions: (optionStruct) => {
+      for(let key in optionStruct) {
+        switch (key) {
+          case 'subcontinent':
+            if (optionStruct['subcontinent'] !== " ") { 
+                mapOptions['region'] = optionStruct['subcontinent'];
             }
-
-            // Organize the data for the map
-            let features = [['Country', data[0].key]]
-            data[0].values.forEach(e => {
-                features.push([e.x[0],e.y]);
-            });
-
-            return features;
-        },
-        /**
-         * @argument dataStruct organized data
-         * @argument options object with map parameters
-         * 
-         * Draw the map and the table.
-         */
-        drawRegionsMap: function (features) {
-            let data = google.visualization.arrayToDataTable(features);
-
-            let chart = new google.visualization.GeoChart(document.getElementById('map_div'));
-            let table = new google.visualization.Table(document.getElementById('table_div'));
-    
-            chart.draw(data, mapOptions);
-            table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
-            
-            google.visualization.events.addListener(table, 'select', () => {
-                chart.setSelection(table.getSelection())
-            });
-            google.visualization.events.addListener(chart, 'select', () => {
-                table.setSelection(chart.getSelection())
-            });
-        },
-        /**
-         * @param optionStruct object containing user inputs
-         *  
-         * Redraw the map
-         */
-        updateMap: function(optionStruct) {
-            this.prepareOptions(optionStruct);
-            this.drawRegionsMap(this.prepareDataMap());
-        },
-        /**
-         * @param optionStruct object containing user inputs
-         * 
-         * Transforms user input into a map configuration
-         */
-        prepareOptions: (optionStruct) => {
-            for(let key in optionStruct) {
-                switch (key) {
-                    case 'subcontinent':
-                        if (optionStruct['subcontinent'] !== " ") { 
-                            mapOptions['region'] = optionStruct['subcontinent'];
-                        }
-                        break;
-                    case 'country':
-                        if (optionStruct['country'] !== " ") { 
-                            mapOptions['region'] = optionStruct['country'];
-                        }
-                        break;
-                    case 'colorAxisFrom' :
-                    case 'colorAxisTo' :
-                        mapOptions['colors'] = [optionStruct['colorAxisFrom'],optionStruct['colorAxisTo']]
-                        break;
-                    default:
-                        mapOptions[key] = optionStruct[key]
-                        break;
-                }
+            break;
+          case 'country':
+            if (optionStruct['country'] !== " ") { 
+                mapOptions['region'] = optionStruct['country'];
             }
-        },
-    })
+            break;
+          case 'colorAxisFrom' :
+          case 'colorAxisTo' :
+            mapOptions['colors'] = [optionStruct['colorAxisFrom'],optionStruct['colorAxisTo']]
+            break;
+          default:
+            mapOptions[key] = optionStruct[key]
+            break;
+        }
+      }
+    },
+    addMapEvents: () => {
+      google.visualization.events.addListener(table, 'select', () => {
+        chart.setSelection(table.getSelection())
+      });
+      google.visualization.events.addListener(chart, 'select', () => {
+        table.setSelection(chart.getSelection())
+      });
+    }
+  })
 });
